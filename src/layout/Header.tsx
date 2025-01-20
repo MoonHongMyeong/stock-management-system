abdcb46690fbe207acf5a7460f815882aef89bb2
MoonHongMyeong <moonhongmyeong@gmail.com>
2025-01-20 22:56:54 +0900

    feat: 동적 헤더 메뉴 구현
    - 활성화된 플랫폼을 헤더 메뉴로 표시
    - 메뉴 스타일링

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,9 +1,28 @@
-import './header.css'
+import { usePlatformStore } from '@/store/platformStore';
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './header.css';
 
 const Header = () => {
+    const { activeMenus, refreshMenus } = usePlatformStore();
+
+    useEffect(() => {
+        refreshMenus();
+    }, []);
+
     return (
         <nav>
             <div className="nav-content">
+                {activeMenus.map((menu) => (
+                    <Link 
+                        key={menu.id} 
+                        to={menu.path}
+                        className="nav-link"
+                    >
+                        {menu.icon && <img src={menu.icon} alt={menu.name} className="nav-icon" />}
+                        <span>{menu.name}</span>
+                    </Link>
+                ))}
             </div>
         </nav>
     )
